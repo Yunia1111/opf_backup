f329493b39bbe4cee3cf03e3b7590b91297b89dd
Oskar <git@oskar.lol>
2025-11-03 17:11:39 +0100

    Add orchestration script to powerflow module

--- README.md
+++ README.md
@@ -30,3 +30,32 @@ python -m venv .				# Create Virtual Env
 . bin/activate					# Activate VEnv
 pip install -r requirements.txt	# Install requirements
 ```
+
+## First Run
+
+```sh
+python -m powerflow <sequence of tasks>
+```
+
+where tasks can be `fetch-db`, `data-prep`, `create-model`, `analysis`.
+
+For example:
+
+```sh
+python -m powerflow fetch-db data-prep create-model
+```
+
+If previous steps have never been executed and therefore their data is missing,
+they are executed beforehand automatically.
+
+They only need to be stated explicitly if you want them to be re-run, e.g. to
+update their data.
+
+So normally, you can just run the target step you're working on:
+
+
+```sh
+python -m powerflow analysis
+```
+
+and everything should work.
---
+++ powerflow/__main__.py
@@ -0,0 +1,51 @@
+import os, sys, time
+
+# Change working dir to module location
+
+os.chdir(sys.path[0])
+
+commands = sys.argv[1:]
+
+command_tests = {
+	'fetch-db': os.path.isfile('data/db_cache/substations.json'),
+	'data-prep': os.path.isfile('data/db_cache/generators_aggregate.json'),
+	'create-model': os.path.isfile('data/intermediate_model/loads.csv'),
+	'analysis': os.path.isfile('results/'),
+}
+
+for command in commands:
+	if command not in command_tests:
+		print(f"Error: Unknown command '{command}'.")
+		exit()
+
+target_command = sys.argv[-1]
+
+insert_counter = 0
+for command in command_tests:
+	if command not in commands and command_tests[command] == False:
+		commands.insert(insert_counter, command)
+		insert_counter += 1
+	if command == target_command:
+		break
+
+command_tasks = {
+	'fetch-db': ['dataminer', 'fetch_db'],
+	'data-prep': ['dataminer', 'prep'],
+	'create-model': ['dataminer', 'model'],
+	'analysis': ['analysis', 'all'],
+}
+
+import importlib
+
+for i, command in enumerate(commands):
+
+	print(f"{i+1}/{len(commands)} Starting task '{command}'...")
+	start_time = time.time()
+
+	module, function = command_tasks[command]
+
+	i = importlib.import_module('powerflow.'+module)
+	getattr(i, function)()
+
+	dm, ds = divmod(time.time() - start_time, 60)
+	print(f"Finished task '{command}' after {dm:.0f}:{ds:02.0f}.")
